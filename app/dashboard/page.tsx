869da5764f3af3a91b179fa1804b25b5d5104b0b
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-02-15 04:47:04 -0500

    Delete page.tsx

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>SSGPT6 Founder Dashboard</h1>
-      <p>Core System Control Panel</p>
-    </div>
-  );
-}
