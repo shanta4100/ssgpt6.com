aecb28939cdf27d2090298869e1b7b4793efacb6
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-02-15 04:08:12 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-<link rel="icon" type="image/png" sizes="192x192" href="/tradehub-icon-192.png" />
-<link rel="icon" type="image/png" sizes="512x512" href="/tradehub-icon-512.png" />
 <link rel="manifest" href="/manifest.webmanifest" />
+<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+<meta name="theme-color" content="#0a0a0a" />
