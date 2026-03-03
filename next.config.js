0479bbe6e103703d9ee64c1c58d659e4139236b6
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-03-03 04:27:02 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,36 @@ const nextConfig = {
   reactStrictMode: true
 };
 
+module.exports = nextConfig;+
+
+> SSGPT6-1f691
+
+</>
+
+Publish
+
+next.config.js X
+
+/** @type {import('next'). NextConfig} */
+
+const nextConfig = {
+
+reactStrictMode: true,
+
+// Increase the timeout for serverless function
+
+serverActions: {
+
+},
+
+bodySizeLimit: '10mb',
+
+experimental: {
+
+serverActionsTimeout: 120, // 2 minutes
+
+}
+
+};
+
 module.exports = nextConfig;
